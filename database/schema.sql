fe117744a0c8a3fe922ac0afc9abba7e44888580
Pete Rai <peterai@cisco.com>
2021-03-23 14:43:30 +0000

    Added users and roles to database schema initialisation

--- database/schema.sql
+++ database/schema.sql
@@ -1,31 +1,56 @@
 
 \connect postgres
 
-DROP DATABASE IF EXISTS bitbroker;
-DROP USER IF EXISTS bitbroker;
+-- clean up any previous assets - for development only
 
-CREATE USER bitbroker WITH ENCRYPTED PASSWORD 'bitbroker';
-CREATE DATABASE bitbroker WITH ENCODING = 'UTF8' OWNER = bitbroker;
+DROP DATABASE IF EXISTS bitbroker;
+
+DROP USER IF EXISTS bbk_adm;
+DROP USER IF EXISTS bbk_api;
+DROP USER IF EXISTS bbk_www;
+
+DROP ROLE IF EXISTS bbk_reader;
+DROP ROLE IF EXISTS bbk_writer;
+
+-- create the user and role assets
+
+CREATE ROLE bbk_reader;
+CREATE ROLE bbk_writer;
+
+CREATE USER bbk_adm WITH ENCRYPTED PASSWORD 'bbk_adm';
+CREATE USER bbk_api WITH ENCRYPTED PASSWORD 'bbk_api';
+CREATE USER bbk_www WITH ENCRYPTED PASSWORD 'bbk_www';
+
+GRANT bbk_reader TO bbk_www;
+GRANT bbk_writer TO bbk_adm, bbk_api;
+
+-- create the database
+
+CREATE DATABASE bitbroker WITH ENCODING = 'UTF8' OWNER = bbk_adm;
 
 \connect bitbroker
 
--- TODO: add DELETE CASCADE?
+-- register table
 
-CREATE TABLE entities
+CREATE TABLE register
 (
     id SERIAL PRIMARY KEY,
     name VARCHAR (255) UNIQUE NOT NULL,
+    description TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMP NULL
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+--  TODO: deleted_at - logical delete?
+--  TODO: add a DELETE CASCADE?
 );
 
--- TODO: not sure how to do best practice user management yet
+-- grant database permissions
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO bitbroker;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO bbk_reader;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO bbk_writer;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO bbk_writer;
 
 -- TODO: obviously the below INSERT is just temporary
 
 INSERT
-  INTO entities (name)
-VALUES ('car_park'), ('bus_stop'), ('school'), ('crime');
+  INTO register (name, description)
+VALUES ('car_park','xxx'), ('bus_stop','xxx'), ('school','xxx'), ('crime','xxx');
