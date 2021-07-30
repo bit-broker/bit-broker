a43be02d88f5f09bb5dee4ee3852a5cdd951660a
jdiaconu <jdiaconu@cisco.com>
2021-07-30 12:12:08 +0200

    [Scripts] Update script to build all images

--- build-docker.sh
+++ build-docker.sh
@@ -1,13 +1,51 @@
+#!/bin/bash -e
+# Syntax build-docker.sh [-i|--image imagename]
 
-docker build --no-cache \
-    -t bbk-consumer:latest \
-    -f ./build/consumer/Dockerfile \
+# Global vars
+######################################################################
+CONTRIBUTOR_IMAGE="bbkr/bbk-contributor:latest"
+COORDINATOR_IMAGE="bbkr/bbk-coordinator:latest"
+CONSUMER_IMAGE="bbkr/bbk-consumer:latest"
+DOCKER_BASE_FOLDER="./build/"
+######################################################################
+
+# Get command
+while [[ $# -gt 0 ]]
+do
+    key="${1}"
+
+    case ${key} in
+    -i|--image)
+        DOCKER_IMAGE="${2}"
+        shift;shift
+        ;;
+    -h|--help)
+        less README.md
+        exit 0
+        ;;
+    *) # unknown
+        echo Unknown Parameter $1
+        exit 4
+    esac
+done
+
+echo BUILDING DOCKER ${CONTRIBUTOR_IMAGE}
+
+docker build \
+    -t ${CONTRIBUTOR_IMAGE} \
+    -f ${DOCKER_BASE_FOLDER}contributor/Dockerfile \
     .
-docker build --no-cache \
-    -t bbk-contributor:latest \
-    -f ./build/contributor/Dockerfile \
+
+echo BUILDING DOCKER ${CONSUMER_IMAGE}
+
+docker build \
+    -t ${CONSUMER_IMAGE} \
+    -f ${DOCKER_BASE_FOLDER}consumer/Dockerfile \
     .
-docker build --no-cache \
-    -t bbk-coordinator:latest \
-    -f ./build/coordinator/Dockerfile \
+
+echo BUILDING DOCKER ${COORDINATOR_IMAGE}
+
+docker build \
+    -t ${COORDINATOR_IMAGE} \
+    -f ${DOCKER_BASE_FOLDER}coordinator/Dockerfile \
     .
