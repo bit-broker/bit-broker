22d9937f146c38b0ad0d87db1ac4297df6ab02cb
Pete Rai <pete@rai.org.uk>
2021-08-25 11:08:27 +0100

    Improved logging in rate limit stub

--- development/stubs/dummy-rate-limit.js
+++ development/stubs/dummy-rate-limit.js
@@ -12,6 +12,7 @@ require('dotenv').config({ path: `${ PATH_CFG }/.env` });
 // --- dependancies
 
 const Server = require(`${ PATH_LIB }/server.js`);
+const log = require(`${ PATH_LIB }/logger.js`).Logger;
 
 // --- running contexts
 
@@ -19,8 +20,15 @@ var api = new Server('dummy rate limit service', process.env.RATE_SERVICE);
 
 // --- dummy endpoints
 
-api.router.put('/:slug/config', (req, res) => res.send());
-api.router.delete('/:slug/config', (req, res) => res.send());
+api.router.put('/:slug/config', (req, res) => {
+    log.info('upsert', req.params.slug, JSON.stringify(req.body))
+    res.send();
+});
+
+api.router.delete('/:slug/config', (req, res) => {
+    log.info('delete', req.params.slug)
+    res.send();
+});
 
 // --- start the server
 
